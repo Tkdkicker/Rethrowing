d92056bee1b30b4d78d4561d1ac0bd3e2abdeb8b
gemma <gemma@DESKTOP-2Q2G917>
2022-11-16 10:13:19 +0000

    Add project files.

---
+++ Rethrowing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Rethrowing
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var s = new Sentence(null);
+            Console.WriteLine($"The first character is {s.GetFirstCharacter()}");
+        }
+    }
+
+    // The example displays the following output:
+    //    Unhandled Exception: System.NullReferenceException: Object reference not set to an instance of an object.
+    //       at Sentence.GetFirstCharacter()
+    //       at Program.Main()
+}
---
+++ Rethrowing/Sentence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Rethrowing
+{
+    public class Sentence
+    {
+        public Sentence(string s)
+        {
+            Value = s;
+        }
+
+        public string Value
+        {
+            get;
+            set;
+        }
+
+        public char GetFirstCharacter()
+        {
+            try
+            {
+                return Value[0];
+            }
+            catch (NullReferenceException e)
+            {
+                throw;
+            }
+        }
+    }
+}
